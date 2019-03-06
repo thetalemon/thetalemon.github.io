01cef074530bfadb22726be5bfcbb7ca713ebf30
thetalemon <sasami0314@gmail.com>
2019-03-06 22:50:41 +0900

    add get double score

--- flaskvue/frontend/src/components/About.vue
+++ flaskvue/frontend/src/components/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div>
-    <p>About</p>
+    <p>Home page</p>
+    <p> num: <input v-model="formData.val1" placeholder="number"></p>
+    <p> number is: {{ returnVal }} </p>
+    <button @click="getDoubleScore">Calc double score</button>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      formData: {
+        val1: 0
+      },
+      returnVal: 0
+    }
+  },
+  methods: {
+    getDoubleScore () {
+      this.val1 = this.getDoubleScoreFromBackend()
+    },
+    getDoubleScoreFromBackend () {
+      const path = `http://localhost:5000/api/testpost`
+      axios.post(path, this.formData)
+        .then(response => {
+          this.returnVal = response.data.result
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
--- flaskvue/run.py
+++ flaskvue/run.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from random import *
 app = Flask(__name__,
                     static_folder = "./dist/static",
                                 template_folder = "./dist")
+
+@app.route("/api/testpost", methods=['POST'])
+def hello():
+    response = {
+        "result": int(request.json["val1"].split()[0])*2
+    }
+    return jsonify(response)
 
 @app.route('/api/random')
 def random_number():
